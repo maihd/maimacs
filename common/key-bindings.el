77fdec47ff349d785478ab7f536601aa80e0428a
maihd <maihd.dev@gmail.com>
2018-08-28 15:33:51 +0700

    Add M-f4 (Alt + F4) key bindings to close selected-frame, then close emacs if there is no frame exists

--- common/key-bindings.el
+++ common/key-bindings.el
@@ -15,6 +15,13 @@
   (forward-line -1)
   (indent-according-to-mode))
 
+(defun close-emacs-or-frame ()
+  (interactive)
+  (if (>= 1 (length (frame-list)))
+      (save-buffers-kill-emacs)
+    (delete-frame (selected-frame))))
+
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (global-set-key [(meta up)] 'move-line-up)
 (global-set-key [(meta down)] 'move-line-down)
+(global-set-key [(meta f4)] 'close-emacs-or-frame)
