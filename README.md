25b89e79a225f2d9fc6c8d78e002ae37c91f982c
maihd <maihd.dev@gmail.com>
2024-08-07 12:43:37 +0700

    Add fennel-mode

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # Introduction
 Emacs extensions, that useful for me, MaiHD.
 
+
 ## Features
 1. Beautiful theme
 2. Open with Emacs (Windows context menu)
 3. C/C++ style based on Visual Studio C++
 4. Header file is c++-mode by default
 
+
 ## Screenshots
 ![MaiMacs](/images/maimacs.png)
 ![Open with Emacs](/images/open-with-emacs.png)
+
+
+## NOTES
+Not work on other version of Emacs 24.
--- init.el
+++ init.el
@@ -1,12 +1,31 @@
 ;;;
-;;; MaiMacs 2017 - 2020, by MaiHD
+;;; MaiMacs 2017 - 2024, by MaiHD
 ;;;
+
+;; Version checking
 
 (defconst maimacs/version "0.1.1"
   "MaiMacs version.")
 
 (defconst maimacs/emacs-min-version "24.0.0"
   "Emacs min version that is supported by MaiMacs.")
+
+(defconst maimacs/emacs-max-version "24.99.99"
+  "Emacs min version that is supported by MaiMacs.")
+
+(cond
+ ((version< emacs-version maimacs/emacs-min-version)
+  (error (concat "Your Emacs version (%s) is too old. "
+                 "Maimacs require Emacs version %s or above.")
+         emacs-version
+         maimacs/emacs-min-version))
+ ((not (version< emacs-version maimacs/emacs-max-version))
+  (error (concat "Your Emacs version (%s) is too new. "
+                 "Maimacs require Emacs version %s and between.")
+         emacs-version
+         mamacs/emacs-max-version)))
+
+;; Initialization
 
 (defconst maimacs/source-files
   '("themes/maiblue-three-theme.el"
@@ -24,69 +43,66 @@
     (urn-mode . "mode/urn-mode.el")
     (glsl-mode . "mode/glsl-mode.el")
     (nasm-mode . "mode/nasm-mode.el")
+    (fennel-mode . "mode/fennel-mode.el")
     (markdown-mode . "mode/markdown-mode.el"))
   "Maimacs' source files")
 
-(if (version< emacs-version maimacs/emacs-min-version)
-    (error (concat "Your Emacs version (%s) is too old. "
-		           "Maimacs require Emacs version %s or above.")
-	       emacs-version
-	       maimacs/emacs-min-version)
-  (defun maimacs-init (maimacs-directory)
-    "Initialize maimacs"
-    
-    ;; Encoding and line ending
-    (set-language-environment "UTF-8")
-    (set-buffer-file-coding-system 'unix t)
-    (setq-default coding-system 'utf-8)
-    (setq-default buffer-file-coding-sytem 'unix)
-    
-    ;; Hide welcome screen
-    (setq inhibit-splash-screen t)
-    (setq inhibit-startup-message t)
-    
-    ;; Load path 
-    (add-to-list 'load-path maimacs-directory)
-    
-    ;; Loading modules function
-    (defun load-modules (modules-list)
-      "Loading modules in maimacs-directory"
-      (when modules-list
-	    (load (expand-file-name (car modules-list)
-				                maimacs-directory))
-	    (load-modules (cdr modules-list))))
-    (load-modules maimacs/source-files)
+(defun maimacs-init (maimacs-directory)
+  "Initialize maimacs"
+  
+  ;; Encoding and line ending
+  (set-language-environment "UTF-8")
+  (set-buffer-file-coding-system 'unix t)
+  (setq-default coding-system 'utf-8)
+  (setq-default buffer-file-coding-sytem 'unix)
+  
+  ;; Hide welcome screen
+  (setq inhibit-splash-screen t)
+  (setq inhibit-startup-message t)
+  
+  ;; Load path 
+  (add-to-list 'load-path maimacs-directory)
+  
+  ;; Loading modules function
+  (defun load-modules (modules-list)
+    "Loading modules in maimacs-directory"
+    (when modules-list
+      (load (expand-file-name (car modules-list)
+                              maimacs-directory))
+      (load-modules (cdr modules-list))))
+  (load-modules maimacs/source-files)
 
-    ;; Autoload for mode
-    (defun autoload-modes (modes-list)
-      "Autoload modes in maimacs-directory"
-      (when modes-list
-        (letrec ((pair (car modes-list))
-                 (mode (car pair))
-                 (file (cdr pair)))
-          (autoload mode (expand-file-name file maimacs-directory)))
-        (autoload-modes (cdr modes-list))))
-    (autoload-modes maimacs/mode-files)
-    
-    ;; Initialize files' auto mode detection
-    (add-to-list 'auto-mode-alist '("\\.glsl\\'" . glsl-mode))
-    (add-to-list 'auto-mode-alist '("\\.vert\\'" . glsl-mode))
-    (add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode))
-    (add-to-list 'auto-mode-alist '("\\.geom\\'" . glsl-mode))
-    (add-to-list 'auto-mode-alist '("\\.h\\'"    .  c++-mode))
-    (add-to-list 'auto-mode-alist '("\\.inl\\'"  .  c++-mode))
-    (add-to-list 'auto-mode-alist '("\\.php\\'"  .  web-mode))
-    (add-to-list 'auto-mode-alist '("\\.lua\\'"  .  lua-mode))
-    (add-to-list 'auto-mode-alist '("\\.urn\\'"  .  urn-mode))
-    (add-to-list 'auto-mode-alist '("\\.asm\\'"  . nasm-mode))
-    (add-to-list 'auto-mode-alist '("\\.s\\'"    . nasm-mode))
-    (add-to-list 'auto-mode-alist '("\\.md\\'"   . markdown-mode))
+  ;; Autoload for mode
+  (defun autoload-modes (modes-list)
+    "Autoload modes in maimacs-directory"
+    (when modes-list
+      (letrec ((pair (car modes-list))
+               (mode (car pair))
+               (file (cdr pair)))
+        (autoload mode (expand-file-name file maimacs-directory)))
+      (autoload-modes (cdr modes-list))))
+  (autoload-modes maimacs/mode-files)
+  
+  ;; Initialize files' auto mode detection
+  (add-to-list 'auto-mode-alist '("\\.glsl\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.vert\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.geom\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.h\\'"    .  c++-mode))
+  (add-to-list 'auto-mode-alist '("\\.inl\\'"  .  c++-mode))
+  (add-to-list 'auto-mode-alist '("\\.php\\'"  .  web-mode))
+  (add-to-list 'auto-mode-alist '("\\.lua\\'"  .  lua-mode))
+  (add-to-list 'auto-mode-alist '("\\.urn\\'"  .  urn-mode))
+  (add-to-list 'auto-mode-alist '("\\.asm\\'"  . nasm-mode))
+  (add-to-list 'auto-mode-alist '("\\.s\\'"    . nasm-mode))
+  (add-to-list 'auto-mode-alist '("\\.fnl\\'"  . fennel-mode))
+  (add-to-list 'auto-mode-alist '("\\.md\\'"   . markdown-mode))
 
-    ;; Initialize interpreters
-    (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+  ;; Initialize interpreters
+  (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
 
-    ;; Change main mode to c-mode
-    (setq initial-major-mode 'c-mode)
-    (setq initial-scratch-message "")))
+  ;; Change main mode to c-mode
+  (setq initial-major-mode 'c-mode)
+  (setq initial-scratch-message ""))
 
 ;; @endfile: maimacs/init.el
