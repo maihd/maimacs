5eb9e12e75c629483ca9d07818dbf6a4b697da5d
maihd.dev <maihd.dev@gmail.com>
2024-08-26 03:01:12 +0700

    some works

--- data/core/init.lua
+++ data/core/init.lua
@@ -1,6 +1,9 @@
 local fennel = require("libs.fennel")
 table.insert(package.loaders or package.searchers, fennel.searcher)
 
+-- Install Fennel to Lua runtime
+fennel.install()
+
 -- Bindings
 fennel.dofile("data/binds/system.fnl")
 
